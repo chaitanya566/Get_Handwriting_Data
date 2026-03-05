800b3bc6e5c7c8ee71d3420dc54d967f94b5ad04
Chaitanya Saradhi Eerla <120102956+chaitanya566@users.noreply.github.com>
2026-03-05 19:33:22 +0530

    Update Next.js configuration for repository settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = "Get_Handwriting_Data";
+
 const nextConfig = {
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
